5fbb25047ed936f0364e75665e49260f58d6fa85
Suttisak Denduangchai <suttisak.lukesp@outlook.com>
2020-03-13 23:30:51 +0700

    UI

---
+++ ThaiDust/Usercontrols/PollutionGraph.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ThaiDust.Usercontrols
+{
+    public sealed partial class PollutionGraph : UserControl
+    {
+        public static readonly DependencyProperty PollutionTypeProperty = DependencyProperty.Register(
+            "PollutionType", typeof(string), typeof(PollutionGraph), new PropertyMetadata(default(string)));
+
+        public string PollutionType
+        {
+            get { return (string) GetValue(PollutionTypeProperty); }
+            set { SetValue(PollutionTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty PollutionValueProperty = DependencyProperty.Register(
+            "PollutionValue", typeof(double), typeof(PollutionGraph), new PropertyMetadata(default(double)));
+
+        public double PollutionValue
+        {
+            get { return (double) GetValue(PollutionValueProperty); }
+            set { SetValue(PollutionValueProperty, value); }
+        }
+
+        public PollutionGraph()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
